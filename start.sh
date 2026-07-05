8a08d4b66fbf16c369f7ca4911f010b15481c3d7
24f2000187 <24f2000187@ds.study.iitm.ac.in>
2026-07-05 15:17:02 +0530

    Ga2

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 redis-server --daemonize yes
 
 # Start the FastAPI application on the port provided by Render (default 10000)
-uvicorn main:app --host 0.0.0.0 --port ${PORT:-10000}
+uvicorn main:app --host 0.0.0.0 --port ${PORT:-10000} 
